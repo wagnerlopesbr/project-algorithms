b332ff8406599b22b1ef38f77a5395c5d5d46765
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-07 19:10:33 -0300

    DONE: Requisite 1/6 v1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,17 @@
-def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def study_schedule(permanence_period, target_time):  # type: ignore
+    if target_time is None:
+        return None
+
+    if not permanence_period or any(
+        len(period) != 2 or not all(isinstance(time, int) for time in period)  # type: ignore
+        for period in permanence_period  # type: ignore
+    ):  # type: ignore
+        return None
+
+    students_present = 0
+
+    for start, end in permanence_period:  # type: ignore
+        if start <= target_time <= end:
+            students_present += 1
+
+    return students_present
