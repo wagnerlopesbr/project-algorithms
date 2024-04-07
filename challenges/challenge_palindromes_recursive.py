a7ad4d2d68a5048b6ae37a4046ba649e11443fad
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-07 20:11:51 -0300

    DONE: Requisite 3/6 v1

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,11 @@
-def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_recursive(word, low_index, high_index):  # type: ignore
+    if not word:
+        return False
+
+    if low_index >= high_index:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, low_index+1, high_index-1)  # type: ignore
+    else:
+        return False
