f5e49871d77a17d11fa496e42d8871fe37fa1508
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-07 19:56:28 -0300

    DONE: Requisite 2/6 v3 Testing -> some scenarios should be tested as well

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -18,10 +18,12 @@ def test_encrypt_message():
 
     assert encrypt_message("message", 3) == "sem_egas"
     assert encrypt_message("message", 5) == "assem_eg"
+    assert encrypt_message("message", 55) == "egassem"
     assert encrypt_message("newmessage", 7) == "ssemwen_ega"
     assert encrypt_message("anothernewmessage", 9) == "enrehtona_egassemw"
 
     assert encrypt_message("message", 2) == "egass_em"
     assert encrypt_message("message", 4) == "ega_ssem"
+    assert encrypt_message("message", 44) == "egassem"
     assert encrypt_message("newmessage", 6) == "egas_semwen"
     assert encrypt_message("anothernewmessage", 8) == "egassemwe_nrehtona"
