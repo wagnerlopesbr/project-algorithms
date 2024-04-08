1e69d9b692628532354d6a7eb277840d45d979c2
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-08 00:06:48 -0300

    DONE: Requisite 5/6 BONUS v1

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,6 @@
-def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def find_duplicate(nums):  # type: ignore
+    s_nums = sorted(nums)  # type: ignore
+
+    return next((s_nums[i] for i in range(1, len(s_nums))  # type: ignore
+                if s_nums[i] == s_nums[i-1]  # type: ignore
+                and s_nums[i] > 0), False)  # type: ignore
