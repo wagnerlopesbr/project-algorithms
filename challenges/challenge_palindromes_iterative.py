bc3366b616a4781364c10a9c67a9b13885f9d395
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-04-08 00:15:51 -0300

    DONE: Requisite 6/6 BONUS v1

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word):  # type: ignore
+    if not word:  # type: ignore
+        return False
+
+    word_length = len(word)
+
+    return not any(word[i] != word[word_length - i - 1]
+                   for i in range(word_length // 2))  # type: ignore
